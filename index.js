7b82f68a99b724e2ff0872106d5e0b2ebb0e49d5
villivald <maxim.villivald@gmail.com>
2021-03-29 17:10:39 +0300

    tehtävä 3.2

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ let persons = [
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
+app.get("/info", (request, response) => {
+  response.send(
+    "Phonebook has info for " +
+      persons.length +
+      " people" +
+      "<br></br>" +
+      new Date()
+  );
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
