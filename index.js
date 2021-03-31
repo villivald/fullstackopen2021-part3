48a21400e9acbf6e74152da0b4000bbdb4eac97b
villivald <maxim.villivald@gmail.com>
2021-03-31 12:02:20 +0300

    tehtävä 3.8

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("content", (req, res) => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
 
 let persons = [
   {
