3527f0f7add24d7401abde17a247b4a108c64de6
villivald <maxim.villivald@gmail.com>
2021-04-02 16:25:23 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
+const url =
+  "mongodb+srv://fullstack:762762762@cluster0.ztsfy.mongodb.net/persons?retryWrites=true&w=majority";
 
 console.log("connecting to", url);
 
