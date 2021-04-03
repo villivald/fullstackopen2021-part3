b5d6db56455752c21fdde5792258b216e57ae71b
villivald <maxim.villivald@gmail.com>
2021-04-03 14:01:11 +0300

    testing

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ const Person = require("./models/person");
 app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
+
+// MORGAN
 morgan.token("content", (req, res) => {
   return JSON.stringify(req.body);
 });
+
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :content"
   )
 );
+
+// ERROR HANDLER
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
 
 // GET ALL PERSONS
 app.get("/api/persons", (request, response) => {
@@ -25,18 +39,26 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-// // DELETE A PERSON
-// app.delete("/api/persons/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   persons = persons.filter((person) => person.id !== id);
-//   response.status(204).end();
-// });
+// DELETE A PERSON
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 // GET A SINGLE PERSON
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 // POST A NEW PERSON
@@ -57,16 +79,43 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-// // INFO PAGE
-// app.get("/info", (request, response) => {
-//   response.send(
-//     "Phonebook has info for " +
-//       persons.length +
-//       " people" +
-//       "<br></br>" +
-//       new Date()
-//   );
-// });
+// UPDATE A PERSON
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    phone: body.phone,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+// INFO PAGE
+app.get("/info", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.send(
+      "Phonebook has info for " +
+        persons.length +
+        " people" +
+        "<br></br>" +
+        new Date()
+    );
+  });
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+// error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
